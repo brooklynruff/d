d24cb3f5aa9e20f7f4432281c10a3f293be60b62
brooklynruff <brooklynruff73@gmail.com>
2025-01-16 23:50:24 -0700

    v6

--- subv6/master-loader.js
+++ subv6/master-loader.js
@@ -1,7 +1,7 @@
 "use strict";
 var scripts = document.getElementsByTagName("script"),
     scriptUrl = scripts[scripts.length - 1].src,
-    root = "https://cdn.jsdelivr.net/gh/brooklynruff/d@main/subv5/",
+    root = "https://cdn.jsdelivr.net/gh/brooklynruff/d@main/subv6/",
     loaders = {
         unity: "unity.js",
         "unity-beta": "unity-beta.js",

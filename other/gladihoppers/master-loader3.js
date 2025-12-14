588dc53aed63f1c516f3028ca161373dbb131c97
brooklynruff <brooklynruff73@gmail.com>
2025-12-14 14:25:08 -0700

    a

---
+++ other/gladihoppers/master-loader3.js
@@ -0,0 +1,21 @@
+"use strict";
+var scripts = document.getElementsByTagName("script"),
+    scriptUrl = scripts[scripts.length - 1].src,
+    root = scriptUrl.split("master-loader3.js")[0],
+    loaders = {
+        unity: "unity.js"
+    };
+if (0 <= window.location.href.indexOf("pokiForceLocalLoader") && (loaders.unity = "./unity.js", root = "./loaders"), !window.config) throw Error("window.config not found");
+var loader = loaders[window.config.loader];
+if (!loader) throw Error('Loader "' + window.config.loader + '" not found');
+if (!window.config.unityWebglLoaderUrl) {
+    var versionSplit = window.config.unityVersion ? window.config.unityVersion.split(".") : [],
+        year = versionSplit[0],
+        minor = versionSplit[1];
+    window.config.unityWebglLoaderUrl = "https://cdn.jsdelivr.net/gh/brooklynruff/d@main/other/gladihoppers/UnityLoader.2019.2.js"
+}
+var sdkScript = document.createElement("script");
+{
+    var i = document.createElement("script");
+    i.src = root + loader, document.body.appendChild(i)
+}

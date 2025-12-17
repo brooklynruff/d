151ad950b42b591b176f78b3d84207da46433969
brooklynruff <brooklynruff73@gmail.com>
2025-12-17 13:02:28 -0700

    a
    a

--- other/wheelsthatarejolly/main2.js
+++ other/wheelsthatarejolly/main2.js
@@ -1,18 +1,14 @@
 function getTitleLabel() {
     const labels = [
-        'Deobfuscated Edition',
-        'Hamburgers!',
-        'Flashless Version',
-        'Shoutouts to Simpleflips',
-        'Jim',
-        'Jacksepticeye, please come back..',
-        'Did you get the fat lady or the old man?',
-        'Happy 12th of July! If it.. actually is 12th of July..'
+        'No Name Games on top',
+        'Now with a better level system',
+        'Make sure to use No Name Games!',
+        'Did you come from No Name Games? You better have...',
+        'Shoutout to Victor! Who\'s Victor?',
     ];
 
     return labels[Math.floor(Math.random() * labels.length)];
 }
-
 
 function addScript(src) {
     const script = document.createElement('script');
@@ -21,13 +17,28 @@ function addScript(src) {
     return new Promise((resolve) => script.onload = resolve);
 }
 
+async function getValidProxy() {
+    let proxies = [
+        "https://script.google.com/macros/s/AKfycbxVXu6E3uuGsGE9x3tKLuotRZWNqDHxLkkgAf2KpHw_5k76g2_ZF_35y7ZlxdZh5so2/exec",
+        "https://script.google.com/macros/s/AKfycbws-htWRaCHbSWh4SxjMDTqHJhtN1H5pI3umWb5VnbFoJfDsZh9hKVgI3Bc1mpdv45h/exec"
+    ]
+
+    for (let i = 0; i < proxies.length; i++) {
+        let text = await (await fetch(proxies[i])).text();
+
+        if (text == "ok") {
+            return proxies;
+        }
+    }
+}
+
 async function loadGame() {
     const parameters = new URLSearchParams(location.search);
     const getUrlParameter = (key) => parameters.get(key);
 
     window.HW_SETTINGS = {
         siteURL: './',
-        corsProxy: localStorage.getItem("proxy") || 'https://corsproxy.io/?url=',
+        corsProxy: getValidProxy(),
         pathPrefix: '',
         titleLabel: getTitleLabel(),
         titleLabelX: 645,
